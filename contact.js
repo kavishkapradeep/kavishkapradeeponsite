3978a6426b9ef540b2eb12319a7d31714d674243
kavishkapradeep <167418257+kavishkapradeep@users.noreply.github.com>
2024-05-05 23:35:15 +0530

    first commit

---
+++ contact.js
@@ -0,0 +1,15 @@
+function home() {
+    location.replace("main.html");
+}
+
+function gitHub() {
+    location.replace("https://github.com/kavishkapradeep")
+}
+
+function linkedIn(){
+    location.replace("https://www.linkedin.com/in/kavishka-pradeep-05968026a/")
+}
+
+function contact(){
+    window.location.replace("contact.html");
+}
